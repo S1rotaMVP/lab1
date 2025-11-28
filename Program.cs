7f8a0b5bd5252a1cb4329d318efeee7322a8de45
Любомир Онисько <lubomyronysko@gmail.com>
2025-11-28 16:40:25 +0200

    лабка 3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace ConsoleApp1
@@ -35,27 +34,13 @@ namespace ConsoleApp1
 
                 switch (choice)
                 {
-                    case "1":
-                        ManageAnimals();
-                        break;
-                    case "2":
-                        ManageSpecies();
-                        break;
-                    case "3":
-                        ManageCaretakers();
-                        break;
-                    case "4":
-                        ManageFeedingSchedule();
-                        break;
-                    case "5":
-                        ManageHealthRecords();
-                        break;
-                    case "6":
-                        FilterBySpecies();
-                        break;
-                    case "7":
-                        PurchaseFeed();
-                        break;
+                    case "1": ManageAnimals(); break;
+                    case "2": ManageSpecies(); break;
+                    case "3": ManageCaretakers(); break;
+                    case "4": ManageFeedingSchedule(); break;
+                    case "5": ManageHealthRecords(); break;
+                    case "6": FilterBySpecies(); break;
+                    case "7": PurchaseFeed(); break;
                     case "8":
                         Console.WriteLine("Вихід з програми. До побачення!");
                         isRunning = false;
@@ -112,16 +97,20 @@ namespace ConsoleApp1
             Console.WriteLine("=== ЗАКУПІВЛЯ КОРМІВ ===");
             Console.ResetColor();
 
-            List<FeedItem> currentPurchase = new List<FeedItem>();
             double budgetLimit = 10000;
             double currentSpent = 0;
 
             Console.Write("Скільки видів корму ви хочете додати? (мінімум 5): ");
             int count;
+
             while (!int.TryParse(Console.ReadLine(), out count) || count < 1)
             {
                 Console.WriteLine("Будь ласка, введіть коректне число (більше 0).");
             }
+
+            FeedItem[] purchases = new FeedItem[count];
+
+            int realCount = 0;
 
             for (int i = 0; i < count; i++)
             {
@@ -134,7 +123,7 @@ namespace ConsoleApp1
                 if (item.Name.Length < 3)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Помилка: Назва надто коротка! Цей товар пропущено.");
+                    Console.WriteLine("Помилка: Назва надто коротка! Товар пропущено.");
                     Console.ResetColor();
                     continue;
                 }
@@ -163,8 +152,10 @@ namespace ConsoleApp1
                 string answer = Console.ReadLine().ToLower();
                 item.IsPremium = (answer == "так" || answer == "yes" || answer == "+");
 
+                purchases[realCount] = item;
+
                 currentSpent += cost;
-                currentPurchase.Add(item);
+                realCount++;
             }
 
             Console.Clear();
@@ -172,14 +163,16 @@ namespace ConsoleApp1
 
             double totalSum = 0;
             double maxPrice = 0;
-            string mostExpensiveName = "";
+            string mostExpensiveName = "Немає";
             int premiumCount = 0;
 
             Console.WriteLine("{0,-15} | {1,-10} | {2,-10} | {3,-10}", "Назва", "Ціна", "Вага", "Сума");
             Console.WriteLine(new string('-', 55));
 
-            foreach (var item in currentPurchase)
+            for (int i = 0; i < realCount; i++)
             {
+                FeedItem item = purchases[i];
+
                 double itemTotal = item.PricePerKg * item.Weight;
                 totalSum += itemTotal;
 
@@ -198,7 +191,7 @@ namespace ConsoleApp1
                     item.Name, item.PricePerKg, item.Weight, itemTotal);
             }
 
-            double averagePrice = currentPurchase.Count > 0 ? totalSum / currentPurchase.Count : 0;
+            double averagePrice = realCount > 0 ? totalSum / realCount : 0;
 
             Console.WriteLine(new string('=', 55));
             Console.ForegroundColor = ConsoleColor.Green;
@@ -224,23 +217,17 @@ namespace ConsoleApp1
             Console.WriteLine("4. Графік годування");
             Console.WriteLine("5. Фіксація стану здоров’я тварин");
             Console.WriteLine("6. Фільтр тварин за видом");
-            Console.WriteLine("7. Закупівля кормів (РОБОЧИЙ ПУНКТ)");
+            Console.WriteLine("7. Закупівля кормів");
             Console.WriteLine("8. Вихід");
             Console.ResetColor();
         }
 
-        static void ManageAnimals()
-        { Console.WriteLine("В розробці..."); Pause(); }
-        static void ManageSpecies()
-        { Console.WriteLine("В розробці..."); Pause(); }
-        static void ManageCaretakers()
-        { Console.WriteLine("В розробці..."); Pause(); }
-        static void ManageFeedingSchedule()
-        { Console.WriteLine("В розробці..."); Pause(); }
-        static void ManageHealthRecords()
-        { Console.WriteLine("В розробці..."); Pause(); }
-        static void FilterBySpecies() 
-        { Console.WriteLine("В розробці..."); Pause(); }
+        static void ManageAnimals() { Console.WriteLine("В розробці..."); Pause(); }
+        static void ManageSpecies() { Console.WriteLine("В розробці..."); Pause(); }
+        static void ManageCaretakers() { Console.WriteLine("В розробці..."); Pause(); }
+        static void ManageFeedingSchedule() { Console.WriteLine("В розробці..."); Pause(); }
+        static void ManageHealthRecords() { Console.WriteLine("В розробці..."); Pause(); }
+        static void FilterBySpecies() { Console.WriteLine("В розробці..."); Pause(); }
 
         static void Pause()
         {
